b384b859880782cf5b0b23b55156e7d9b8d2956b
Alexandra Sos <alexandra.d.sos@gmail.com>
2014-11-13 19:00:26 -0800

    Add ability to edit and update users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  describe "passwords" do
-    it "needs a password and confirmation to save" do
-      u = User.new(name: "steve")
 
-      u.save
-      expect(u).to_not be_valid
+  it "requires an email" do
+    u = User.new(name: "steve",
+               password: "hunter2",
+               password_confirmation: "hunter2")
 
-      u.password = "password"
-      u.password_confirmation = ""
-      u.save
-      expect(u).to_not be_valid
+    u.save
+    expect(u).to_not be_valid
 
-      u.password_confirmation = "password"
-      u.save
-      expect(u).to be_valid
-		end
+    u.email = "steve@example.com"
+    u.save
+    expect(u).to be_valid
+  end
+
+    describe "passwords" do
+      it "needs a password and confirmation to save" do
+        u = User.new(name: "steve", email: "steve@example.com")
+
+        u.save
+        expect(u).to_not be_valid
+
+        u.password = "password"
+        u.password_confirmation = ""
+        u.save
+        expect(u).to_not be_valid
+
+        u.password_confirmation = "password"
+        u.save
+        expect(u).to be_valid
+      end
+
+
 
     it "needs password and confirmation to match" do
       u = User.create(
